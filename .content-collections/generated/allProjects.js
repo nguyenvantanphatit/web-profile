965bb9e34cc8e5982a981a5e4528026fe4e93821
Tấn Phát <nguyenvantanphat.it@gmail.com>
2024-10-31 00:13:13 +0700

    first commit

---
+++ .content-collections/generated/allProjects.js
@@ -0,0 +1,284 @@
+export default [
+  {
+    "content": "This is a website offering interactive coding tutorials and challenges to help users learn programming languages through practice. The idea was to create a hands-on learning environment where users can improve their coding skills by solving challenges. This project enhanced my ability to create educational content and interactive web applications.",
+    "title": "Interactive Coding Tutorial Site",
+    "description": "A website offering interactive coding tutorials and challenges to help users learn programming languages through practice.",
+    "website": "https://example.com/",
+    "github": "https://github.com/",
+    "tags": [
+      {
+        "label": "HTML"
+      },
+      {
+        "label": "CSS"
+      },
+      {
+        "label": "JavaScript"
+      },
+      {
+        "label": "React"
+      },
+      {
+        "label": "Node.js"
+      },
+      {
+        "label": "MongoDB"
+      }
+    ],
+    "date": "2024-05-26",
+    "_meta": {
+      "filePath": "coding_tutorials.mdx",
+      "fileName": "coding_tutorials.mdx",
+      "directory": ".",
+      "extension": "mdx",
+      "path": "coding_tutorials"
+    },
+    "toc": [],
+    "structuredData": {
+      "contents": [
+        {
+          "heading": "",
+          "content": "This is a website offering interactive coding tutorials and challenges to help users learn programming languages through practice. The idea was to create a hands-on learning environment where users can improve their coding skills by solving challenges. This project enhanced my ability to create educational content and interactive web applications."
+        }
+      ],
+      "headings": []
+    },
+    "body": "var Component=(()=>{var d=Object.create;var i=Object.defineProperty;var g=Object.getOwnPropertyDescriptor;var p=Object.getOwnPropertyNames;var m=Object.getPrototypeOf,f=Object.prototype.hasOwnProperty;var x=(e,n)=>()=>(n||e((n={exports:{}}).exports,n),n.exports),v=(e,n)=>{for(var t in n)i(e,t,{get:n[t],enumerable:!0})},c=(e,n,t,r)=>{if(n&&typeof n==\"object\"||typeof n==\"function\")for(let a of p(n))!f.call(e,a)&&a!==t&&i(e,a,{get:()=>n[a],enumerable:!(r=g(n,a))||r.enumerable});return e};var w=(e,n,t)=>(t=e!=null?d(m(e)):{},c(n||!e||!e.__esModule?i(t,\"default\",{value:e,enumerable:!0}):t,e)),_=e=>c(i({},\"__esModule\",{value:!0}),e);var l=x((y,s)=>{s.exports=_jsx_runtime});var b={};v(b,{default:()=>u});var o=w(l());function h(e){let n={p:\"p\",...e.components};return(0,o.jsx)(n.p,{children:\"This is a website offering interactive coding tutorials and challenges to help users learn programming languages through practice. The idea was to create a hands-on learning environment where users can improve their coding skills by solving challenges. This project enhanced my ability to create educational content and interactive web applications.\"})}function u(e={}){let{wrapper:n}=e.components||{};return n?(0,o.jsx)(n,{...e,children:(0,o.jsx)(h,{...e})}):h(e)}return _(b);})();\n;return Component;"
+  },
+  {
+    "content": "This is an e-commerce platform dedicated to eco-friendly products where users can buy, sell, and learn about sustainable living. The goal of this project was to promote sustainable living by providing a marketplace for eco-friendly products. Working on this project helped me understand the intricacies of e-commerce development and the importance of sustainability in business.",
+    "title": "Eco-friendly Product Marketplace",
+    "description": "An e-commerce platform dedicated to eco-friendly products where users can buy, sell, and learn about sustainable living.",
+    "website": "https://example.com/",
+    "github": "https://github.com/",
+    "tags": [
+      {
+        "label": "HTML"
+      },
+      {
+        "label": "CSS"
+      },
+      {
+        "label": "JavaScript"
+      },
+      {
+        "label": "React"
+      },
+      {
+        "label": "Node.js"
+      },
+      {
+        "label": "Express"
+      },
+      {
+        "label": "MongoDB"
+      }
+    ],
+    "date": "2024-05-26",
+    "_meta": {
+      "filePath": "eco_marketplace.mdx",
+      "fileName": "eco_marketplace.mdx",
+      "directory": ".",
+      "extension": "mdx",
+      "path": "eco_marketplace"
+    },
+    "toc": [],
+    "structuredData": {
+      "contents": [
+        {
+          "heading": "",
+          "content": "This is an e-commerce platform dedicated to eco-friendly products where users can buy, sell, and learn about sustainable living. The goal of this project was to promote sustainable living by providing a marketplace for eco-friendly products. Working on this project helped me understand the intricacies of e-commerce development and the importance of sustainability in business."
+        }
+      ],
+      "headings": []
+    },
+    "body": "var Component=(()=>{var m=Object.create;var i=Object.defineProperty;var p=Object.getOwnPropertyDescriptor;var f=Object.getOwnPropertyNames;var h=Object.getPrototypeOf,b=Object.prototype.hasOwnProperty;var x=(e,n)=>()=>(n||e((n={exports:{}}).exports,n),n.exports),y=(e,n)=>{for(var t in n)i(e,t,{get:n[t],enumerable:!0})},a=(e,n,t,s)=>{if(n&&typeof n==\"object\"||typeof n==\"function\")for(let o of f(n))!b.call(e,o)&&o!==t&&i(e,o,{get:()=>n[o],enumerable:!(s=p(n,o))||s.enumerable});return e};var g=(e,n,t)=>(t=e!=null?m(h(e)):{},a(n||!e||!e.__esModule?i(t,\"default\",{value:e,enumerable:!0}):t,e)),j=e=>a(i({},\"__esModule\",{value:!0}),e);var d=x((w,c)=>{c.exports=_jsx_runtime});var _={};y(_,{default:()=>u});var r=g(d());function l(e){let n={p:\"p\",...e.components};return(0,r.jsx)(n.p,{children:\"This is an e-commerce platform dedicated to eco-friendly products where users can buy, sell, and learn about sustainable living. The goal of this project was to promote sustainable living by providing a marketplace for eco-friendly products. Working on this project helped me understand the intricacies of e-commerce development and the importance of sustainability in business.\"})}function u(e={}){let{wrapper:n}=e.components||{};return n?(0,r.jsx)(n,{...e,children:(0,r.jsx)(l,{...e})}):l(e)}return j(_);})();\n;return Component;"
+  },
+  {
+    "content": "This is a mobile app that helps users track their fitness activities, set goals, and monitor progress with motivational tools. I created this app to encourage people to lead a healthier lifestyle by making fitness tracking simple and engaging. The project allowed me to delve into mobile development and learn more about user-centered design and data management.",
+    "title": "Fitness Tracker App",
+    "description": "A mobile app that helps users track their fitness activities, set goals, and monitor progress with motivational tools.",
+    "website": "https://example.com/",
+    "github": "https://github.com/",
+    "tags": [
+      {
+        "label": "HTML"
+      },
+      {
+        "label": "CSS"
+      },
+      {
+        "label": "JavaScript"
+      },
+      {
+        "label": "React Native"
+      },
+      {
+        "label": "Node.js"
+      },
+      {
+        "label": "MongoDB"
+      }
+    ],
+    "date": "2024-05-26",
+    "_meta": {
+      "filePath": "fitness_tracker.mdx",
+      "fileName": "fitness_tracker.mdx",
+      "directory": ".",
+      "extension": "mdx",
+      "path": "fitness_tracker"
+    },
+    "toc": [],
+    "structuredData": {
+      "contents": [
+        {
+          "heading": "",
+          "content": "This is a mobile app that helps users track their fitness activities, set goals, and monitor progress with motivational tools. I created this app to encourage people to lead a healthier lifestyle by making fitness tracking simple and engaging. The project allowed me to delve into mobile development and learn more about user-centered design and data management."
+        }
+      ],
+      "headings": []
+    },
+    "body": "var Component=(()=>{var p=Object.create;var a=Object.defineProperty;var h=Object.getOwnPropertyDescriptor;var u=Object.getOwnPropertyNames;var g=Object.getPrototypeOf,f=Object.prototype.hasOwnProperty;var x=(e,t)=>()=>(t||e((t={exports:{}}).exports,t),t.exports),_=(e,t)=>{for(var n in t)a(e,n,{get:t[n],enumerable:!0})},r=(e,t,n,s)=>{if(t&&typeof t==\"object\"||typeof t==\"function\")for(let o of u(t))!f.call(e,o)&&o!==n&&a(e,o,{get:()=>t[o],enumerable:!(s=h(t,o))||s.enumerable});return e};var b=(e,t,n)=>(n=e!=null?p(g(e)):{},r(t||!e||!e.__esModule?a(n,\"default\",{value:e,enumerable:!0}):n,e)),j=e=>r(a({},\"__esModule\",{value:!0}),e);var c=x((w,l)=>{l.exports=_jsx_runtime});var v={};_(v,{default:()=>d});var i=b(c());function m(e){let t={p:\"p\",...e.components};return(0,i.jsx)(t.p,{children:\"This is a mobile app that helps users track their fitness activities, set goals, and monitor progress with motivational tools. I created this app to encourage people to lead a healthier lifestyle by making fitness tracking simple and engaging. The project allowed me to delve into mobile development and learn more about user-centered design and data management.\"})}function d(e={}){let{wrapper:t}=e.components||{};return t?(0,i.jsx)(t,{...e,children:(0,i.jsx)(m,{...e})}):m(e)}return j(v);})();\n;return Component;"
+  },
+  {
+    "content": "This is my personal portfolio website where I showcase my coding projects, resume, and skills in a beautifully designed format. I created this website to have an online presence and to share my work with potential employers and clients. The project helped me improve my front-end development skills and learn more about modern web development practices.",
+    "title": "Personal Portfolio Website",
+    "description": "A personal portfolio to showcase your coding projects, resume, and skills in a beautifully designed format.",
+    "website": "https://example.com/",
+    "github": "https://github.com/",
+    "tags": [
+      {
+        "label": "HTML"
+      },
+      {
+        "label": "CSS"
+      },
+      {
+        "label": "JavaScript"
+      },
+      {
+        "label": "React"
+      },
+      {
+        "label": "Next.js"
+      },
+      {
+        "label": "MDX"
+      }
+    ],
+    "date": "2024-05-26",
+    "_meta": {
+      "filePath": "portfolio.mdx",
+      "fileName": "portfolio.mdx",
+      "directory": ".",
+      "extension": "mdx",
+      "path": "portfolio"
+    },
+    "toc": [],
+    "structuredData": {
+      "contents": [
+        {
+          "heading": "",
+          "content": "This is my personal portfolio website where I showcase my coding projects, resume, and skills in a beautifully designed format. I created this website to have an online presence and to share my work with potential employers and clients. The project helped me improve my front-end development skills and learn more about modern web development practices."
+        }
+      ],
+      "headings": []
+    },
+    "body": "var Component=(()=>{var p=Object.create;var r=Object.defineProperty;var h=Object.getOwnPropertyDescriptor;var u=Object.getOwnPropertyNames;var f=Object.getPrototypeOf,w=Object.prototype.hasOwnProperty;var y=(e,n)=>()=>(n||e((n={exports:{}}).exports,n),n.exports),x=(e,n)=>{for(var t in n)r(e,t,{get:n[t],enumerable:!0})},a=(e,n,t,i)=>{if(n&&typeof n==\"object\"||typeof n==\"function\")for(let o of u(n))!w.call(e,o)&&o!==t&&r(e,o,{get:()=>n[o],enumerable:!(i=h(n,o))||i.enumerable});return e};var b=(e,n,t)=>(t=e!=null?p(f(e)):{},a(n||!e||!e.__esModule?r(t,\"default\",{value:e,enumerable:!0}):t,e)),j=e=>a(r({},\"__esModule\",{value:!0}),e);var l=y((k,c)=>{c.exports=_jsx_runtime});var _={};x(_,{default:()=>d});var s=b(l());function m(e){let n={p:\"p\",...e.components};return(0,s.jsx)(n.p,{children:\"This is my personal portfolio website where I showcase my coding projects, resume, and skills in a beautifully designed format. I created this website to have an online presence and to share my work with potential employers and clients. The project helped me improve my front-end development skills and learn more about modern web development practices.\"})}function d(e={}){let{wrapper:n}=e.components||{};return n?(0,s.jsx)(n,{...e,children:(0,s.jsx)(m,{...e})}):m(e)}return j(_);})();\n;return Component;"
+  },
+  {
+    "content": "This is a social app for food lovers where users can post, share, and discover recipes from around the world. I built this app to combine my passion for food and technology, creating a platform where people can connect and share their culinary creations. The project allowed me to work on both front-end and back-end development, enhancing my full-stack development skills.",
+    "title": "Recipe Sharing App",
+    "description": "A social app for food lovers where users can post, share, and discover recipes from around the world.",
+    "website": "https://example.com/",
+    "github": "https://github.com/",
+    "tags": [
+      {
+        "label": "HTML"
+      },
+      {
+        "label": "CSS"
+      },
+      {
+        "label": "JavaScript"
+      },
+      {
+        "label": "React"
+      },
+      {
+        "label": "Node.js"
+      },
+      {
+        "label": "MongoDB"
+      }
+    ],
+    "date": "2024-05-26",
+    "_meta": {
+      "filePath": "recipe_app.mdx",
+      "fileName": "recipe_app.mdx",
+      "directory": ".",
+      "extension": "mdx",
+      "path": "recipe_app"
+    },
+    "toc": [],
+    "structuredData": {
+      "contents": [
+        {
+          "heading": "",
+          "content": "This is a social app for food lovers where users can post, share, and discover recipes from around the world. I built this app to combine my passion for food and technology, creating a platform where people can connect and share their culinary creations. The project allowed me to work on both front-end and back-end development, enhancing my full-stack development skills."
+        }
+      ],
+      "headings": []
+    },
+    "body": "var Component=(()=>{var h=Object.create;var r=Object.defineProperty;var m=Object.getOwnPropertyDescriptor;var f=Object.getOwnPropertyNames;var u=Object.getPrototypeOf,w=Object.prototype.hasOwnProperty;var x=(e,n)=>()=>(n||e((n={exports:{}}).exports,n),n.exports),y=(e,n)=>{for(var o in n)r(e,o,{get:n[o],enumerable:!0})},s=(e,n,o,c)=>{if(n&&typeof n==\"object\"||typeof n==\"function\")for(let t of f(n))!w.call(e,t)&&t!==o&&r(e,t,{get:()=>n[t],enumerable:!(c=m(n,t))||c.enumerable});return e};var _=(e,n,o)=>(o=e!=null?h(u(e)):{},s(n||!e||!e.__esModule?r(o,\"default\",{value:e,enumerable:!0}):o,e)),b=e=>s(r({},\"__esModule\",{value:!0}),e);var l=x((v,i)=>{i.exports=_jsx_runtime});var j={};y(j,{default:()=>p});var a=_(l());function d(e){let n={p:\"p\",...e.components};return(0,a.jsx)(n.p,{children:\"This is a social app for food lovers where users can post, share, and discover recipes from around the world. I built this app to combine my passion for food and technology, creating a platform where people can connect and share their culinary creations. The project allowed me to work on both front-end and back-end development, enhancing my full-stack development skills.\"})}function p(e={}){let{wrapper:n}=e.components||{};return n?(0,a.jsx)(n,{...e,children:(0,a.jsx)(d,{...e})}):d(e)}return b(j);})();\n;return Component;"
+  },
+  {
+    "content": "This platform allows students to form virtual study groups, schedule sessions, and share resources in real-time. The idea behind this project was to provide a collaborative environment for students to study together, even when they are physically apart. Working on this project helped me understand real-time communication and the importance of user experience in a collaborative tool.",
+    "title": "Virtual Study Group Platform",
+    "description": "A platform where students can form virtual study groups, schedule sessions, and share resources in real-time.",
+    "website": "https://example.com/",
+    "github": "https://github.com/",
+    "tags": [
+      {
+        "label": "HTML"
+      },
+      {
+        "label": "CSS"
+      },
+      {
+        "label": "JavaScript"
+      },
+      {
+        "label": "React"
+      },
+      {
+        "label": "Node.js"
+      },
+      {
+        "label": "Socket.IO"
+      },
+      {
+        "label": "MongoDB"
+      }
+    ],
+    "date": "2024-05-26",
+    "_meta": {
+      "filePath": "study_group.mdx",
+      "fileName": "study_group.mdx",
+      "directory": ".",
+      "extension": "mdx",
+      "path": "study_group"
+    },
+    "toc": [],
+    "structuredData": {
+      "contents": [
+        {
+          "heading": "",
+          "content": "This platform allows students to form virtual study groups, schedule sessions, and share resources in real-time. The idea behind this project was to provide a collaborative environment for students to study together, even when they are physically apart. Working on this project helped me understand real-time communication and the importance of user experience in a collaborative tool."
+        }
+      ],
+      "headings": []
+    },
+    "body": "var Component=(()=>{var h=Object.create;var r=Object.defineProperty;var m=Object.getOwnPropertyDescriptor;var p=Object.getOwnPropertyNames;var f=Object.getPrototypeOf,x=Object.prototype.hasOwnProperty;var v=(e,t)=>()=>(t||e((t={exports:{}}).exports,t),t.exports),y=(e,t)=>{for(var n in t)r(e,n,{get:t[n],enumerable:!0})},i=(e,t,n,a)=>{if(t&&typeof t==\"object\"||typeof t==\"function\")for(let o of p(t))!x.call(e,o)&&o!==n&&r(e,o,{get:()=>t[o],enumerable:!(a=m(t,o))||a.enumerable});return e};var j=(e,t,n)=>(n=e!=null?h(f(e)):{},i(t||!e||!e.__esModule?r(n,\"default\",{value:e,enumerable:!0}):n,e)),_=e=>i(r({},\"__esModule\",{value:!0}),e);var l=v((g,c)=>{c.exports=_jsx_runtime});var w={};y(w,{default:()=>d});var s=j(l());function u(e){let t={p:\"p\",...e.components};return(0,s.jsx)(t.p,{children:\"This platform allows students to form virtual study groups, schedule sessions, and share resources in real-time. The idea behind this project was to provide a collaborative environment for students to study together, even when they are physically apart. Working on this project helped me understand real-time communication and the importance of user experience in a collaborative tool.\"})}function d(e={}){let{wrapper:t}=e.components||{};return t?(0,s.jsx)(t,{...e,children:(0,s.jsx)(u,{...e})}):u(e)}return _(w);})();\n;return Component;"
+  }
+];
